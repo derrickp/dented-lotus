cc4f96fc64c5506c113b39feb26cd85884e558ad
Derrick Plotsky <derrickplotsky@gmail.com>
2017-03-05 19:57:05 -0800

    Delete unused file

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -11,6 +11,7 @@ var __extends = (this && this.__extends) || (function () {
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = require("react");
+var Pages_1 = require("../components/Pages");
 var User_1 = require("./User");
 var burger = require("react-burger-menu");
 var Menu = burger.slide;
@@ -33,9 +34,10 @@ var MenuComponent = (function (_super) {
         }
         return React.createElement(Menu, { width: 270, customBurgerIcon: false, pageWrapId: "page-wrap", isOpen: this.props.isOpen, right: true },
             React.createElement(User_1.UserComponent, { small: true, stateManager: this.props.stateManager }),
-            React.createElement("a", { id: "home", className: "menu-item", href: "#home", onClick: function () { return _this.onPageChange("home"); } }, "Home"),
-            React.createElement("a", { id: "races", className: "menu-item", href: "#page=all-races", onClick: function () { return _this.onPageChange("all-races"); } }, "Races"),
-            React.createElement("a", { id: "tracks", className: "menu-item", href: "#page=tracks", onClick: function () { return _this.onPageChange("tracks"); } }, "Tracks"));
+            React.createElement("a", { id: "home", className: "menu-item", href: "#home", onClick: function () { return _this.onPageChange(Pages_1.Pages.HOME); } }, "Home"),
+            React.createElement("a", { id: "races", className: "menu-item", href: "#page=all-races", onClick: function () { return _this.onPageChange(Pages_1.Pages.ALL_RACES); } }, "Races"),
+            React.createElement("a", { id: "tracks", className: "menu-item", href: "#page=tracks", onClick: function () { return _this.onPageChange(Pages_1.Pages.TRACKS); } }, "Tracks"),
+            React.createElement("a", { id: "drivers", className: "menu-item", href: "#page=drivers", onClick: function () { return _this.onPageChange(Pages_1.Pages.DRIVERS); } }, "Drivers"));
     };
     return MenuComponent;
 }(React.Component));
