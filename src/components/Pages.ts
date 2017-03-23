d00037914ebc125f3a7139119c19484f26e085a0
Derrick Plotsky <derrickplotsky@gmail.com>
2017-03-22 22:29:51 -0700

    React Bootstrap Navbar
     - Swapping out Banner for React Bootstrap Navbar
     - Slimming down LoginLogout
     - Removing unnecessary Modal
     - Removing unnecessary Signup

--- src/components/Pages.ts
+++ src/components/Pages.ts
@@ -3,10 +3,9 @@ import { AllRaces } from "./Pages/AllRaces";
 import { TrackPage } from "./Pages/TrackPage";
 import { Tracks } from "./Pages/Tracks";
 import {Drivers} from "./Pages/Drivers";
-import { Signup } from "./Pages/Signup";
 import { AllSeasonPicks } from "./Pages/AllSeasonPicks"
 
-export { RacePage, AllRaces, TrackPage, Tracks, Drivers, Signup, AllSeasonPicks };
+export { RacePage, AllRaces, TrackPage, Tracks, Drivers, AllSeasonPicks };
 
 export class Pages {
     static HOME = "home";
@@ -15,7 +14,8 @@ export class Pages {
     static DRIVERS = "drivers"; 
     static TRACKS = "tracks";
     static ALL_RACES = "all-races";
-    static SIGN_UP = "signup";
     static PICKS = "picks";
     static ALL_SEASON_PICKS = "all-season-picks";
+    static BLOGS = "blogs";
+    static PROFILE = "profile";
 }
--- src/components/widgets/LoginLogout.tsx
+++ src/components/widgets/LoginLogout.tsx
@@ -1,6 +1,5 @@
 
 import * as React from "react";
-import { User, FacebookUser, GoogleUser } from "../../../common/models/User";
 import Rodal from "rodal";
 import { Pages } from "../Pages";
 import { MenuComponent } from "../Menu";
@@ -9,19 +8,14 @@ import { FacebookLogin } from "../Auth/FacebookLogin";
 import { Modal, Button } from "react-bootstrap";
 
 export interface LoginLogoutProps {
-    onPageChange: (page: string) => void;
     doGoogleLogin: () => Promise<void>;
     doFacebookLogin: () => Promise<void>;
-    logout: () => void;
-    signUp: (type: string) => void;
     loggedIn: boolean;
-    user: User;
+    show: boolean;
+    onLogin: () => void;
 }
 
 export interface LoginLogoutState {
-    sidebarOpen: boolean;
-    modalVisible: boolean;
-    loading: boolean;
 }
 
 export class LoginLogout extends React.Component<LoginLogoutProps, LoginLogoutState>{
@@ -31,14 +25,13 @@ export class LoginLogout extends React.Component<LoginLogoutProps, LoginLogoutSt
     constructor(props: LoginLogoutProps) {
         super(props);
         this.state = { modalVisible: false, sidebarOpen: false, loading: false };
-        this.onLogin = this.onLogin.bind(this);
         this.hideLoginModal = this.hideLoginModal.bind(this);
         this.googleLogin = this.googleLogin.bind(this);
         this.fbLogin = this.fbLogin.bind(this);
     }
 
     googleLogin() {
-        this.setState({loading: true});
+        this.setState({ loading: true });
         this.props.doGoogleLogin().then(() => {
             this.setState({ loading: false });
             this.hideLoginModal();
@@ -53,54 +46,24 @@ export class LoginLogout extends React.Component<LoginLogoutProps, LoginLogoutSt
         });
     }
 
-    showLoginModal() {
-        this.setState({ modalVisible: true });
-    }
-
     hideLoginModal() {
-        this.setState({ modalVisible: false });
-    }
-
-    onMenuClicked() {
-        let next = !this.state.sidebarOpen;
-        this.setState({ sidebarOpen: next });
-    }
-
-    onLogin() {
-        this.hideLoginModal();
-    }
-
-    onSignUp(type: string) {
-        this.hideLoginModal();
-        this.props.signUp(type);
+        this.props.onLogin();
     }
 
     render() {
-        let sidebarContent = "<b>Sidebar content</b>";
         const googleLogin = this.props.doGoogleLogin ? this.googleLogin : null;
         const fbLogin = this.props.doFacebookLogin ? this.fbLogin : null;
-        if (this.props.loggedIn) {
-            return <div className="logout" onClick={this.onMenuClicked.bind(this)}>
-                <MenuComponent user={this.props.user} onPageChange={this.props.onPageChange} isOpen={this.state.sidebarOpen} onLogout={this.props.logout} />
-            </div>
-        } else {
-            return (
-                <div className="login">
-                    <span onClick={this.showLoginModal.bind(this)}>Log In / Sign Up</span>
-                    <Modal show={this.state.modalVisible} onHide={this.hideLoginModal} >
-                        <Modal.Header closeButton={true}>
-                            <Modal.Title>Log In or Sign Up With</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <GoogleLogin login={googleLogin} loggedIn={this.props.loggedIn} />
-                            <FacebookLogin loggedIn={this.props.loggedIn} login={fbLogin}></FacebookLogin>
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button onClick={this.hideLoginModal}>Close</Button>
-                        </Modal.Footer>
-                    </Modal>
-                </div>
-            );
-        }
+        return <Modal show={this.props.show} onHide={this.hideLoginModal} >
+            <Modal.Header closeButton={true}>
+                <Modal.Title>Log In or Sign Up With</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <GoogleLogin login={googleLogin} loggedIn={this.props.loggedIn} />
+                <FacebookLogin loggedIn={this.props.loggedIn} login={fbLogin}></FacebookLogin>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={this.hideLoginModal}>Close</Button>
+            </Modal.Footer>
+        </Modal>;
     }
 }
