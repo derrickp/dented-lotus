306dbaf3e1781ca6dfbec8bdadf423c82ddf61f7
cisenor1 <cisenor1@gmail.com>
2017-03-23 12:27:00 -0700

    Add meta tag Css for DriverComponent.tsx

--- src/components/widgets/DriverComponent.tsx
+++ src/components/widgets/DriverComponent.tsx
@@ -118,7 +118,7 @@ function DisplayDriver(props: EditDriverProps) {
                     <label htmlFor="form-field-6">Team</label>
                 </div>
                 <div className="small-driver-info col-md-3">
-                    <img className="driver-image" src={imageUrl} />
+                    <img className="img-responsive center-block" src={imageUrl} />
                 </div>
             </div>
                 
