8b5df23cb96e8a890566166bc103bc669557f375
Derrick Plotsky <derrickplotsky@gmail.com>
2017-03-27 23:01:40 -0700

    Console log

--- app/utilities/userFunctions.ts
+++ app/utilities/userFunctions.ts
@@ -60,6 +60,7 @@ function verifyGoogleId(token: string): Promise<GoogleAuthResponse> {
             //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3],
             (error, login) => {
                 if (error) {
+                    console.log(error);
                     reject(error);
                     return;
                 }
